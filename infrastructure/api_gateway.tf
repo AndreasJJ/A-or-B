37a04c77ac9503d4653781e48c2b037d3039646a
Andreasjj <andreasjj@gmail.com>
2020-09-12 16:12:36 +0200

    Added terraform for frontend

---
+++ infrastructure/api_gateway.tf
@@ -0,0 +1,47 @@
+resource "aws_api_gateway_rest_api" "api" {
+ name = "api-gateway"
+ description = "Proxy to handle requests to our API"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_rest_api.api.root_resource_id
+  path_part = "/"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_resource.resource.id
+  path_part = "api"
+}
+
+resource "aws_api_gateway_resource" "auth_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_resource.resource.id
+  path_part = "auth"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "https://${local.application_domain}/{proxy}"
+ 
+  request_parameters =  {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
