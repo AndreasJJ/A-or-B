b345c881f82d3f4257f170c7e8f2b5c08c5a0a3b
Andreasjj <andreasjj@gmail.com>
2020-10-03 21:20:29 +0200

    minor changes

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id = aws_api_gateway_rest_api.api.root_resource_id
-  path_part = "/"
+  path_part = "$default"
 }
 
 resource "aws_api_gateway_resource" "api_resource" {
